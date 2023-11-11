4510e2377f55e2876f715e30bdd4373699eab6c4
Ofir Steinherz <ofir.steinherz@gmail.com>
2023-11-11 21:43:01 +0000

    Upload GPiT to PyPI

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Read the content of README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='gpit',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gpit=gpit.main:main',
+        ],
+    },
+    # Additional metadata
+    author='Ofir Steinherz',
+    author_email='ofir.steinherz@gmail.com',
+    description='GPT-Powered Commit Assistance',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
